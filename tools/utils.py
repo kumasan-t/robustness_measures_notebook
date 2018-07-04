0153e96497a467f1e6e5f4b69d3649138558aa79
randomBEAR <lorenzo.travagliati@gmail.com>
2018-07-04 17:20:41 +0200

    Upgraded project with new tools and functions
    New tools:
    - build_presence_dictionary
    - remove_satellites
    - betweenness_centrality_intersection
    - read_k_conn_from_files
    
    New visualization functions that show:
    - the daily average
    - best nodes wrt betweenness centrality
    - gantt chart that accounts for the node presence in the top 20 chart

--- tools/utils.py
+++ tools/utils.py
@@ -1,4 +1,78 @@
 import networkx as nx
+import collections as ct
+import glob
+import json
+import os
+
+
+def build_presence_dictionary(betweenness_centrality_lists):
+    """
+    Builds a presence list that counts how many time a node appears in the top 15 nodes depending on the
+    score of the betweenness centrality.
+    :param betweenness_centrality_lists:  a dictionary containing all the bc values for each snapshot
+    :return: a dictionary of list
+    """
+    number_of_snapshot = len(betweenness_centrality_lists)
+
+    # Sort each snapshot, select the best 20 nodes.
+    sorted_snapshots = [None] * number_of_snapshot
+    presence_count = ct.defaultdict(list)
+    for i in range(number_of_snapshot):
+        sorted_snapshots[i] = sorted(betweenness_centrality_lists[i].items(), key=lambda x: x[1], reverse=True)
+        top_fifteen = [sorted_snapshots[i][j] for j in range(15)]
+        for item in top_fifteen:
+            presence_count[item[0]].append(i)
+    return presence_count
+
+
+def removes_satellites(graph):
+    """
+    Recursive function, removes from the graph every satellite recursively until all the satellites are removed.
+    :param graph: a networkx graph
+    :return: the number of nodes removed
+    """
+    nodes_removed = 0
+    for node in list(graph.nodes):
+        if nx.degree(graph, node) == 1:
+            nodes_removed += 1
+            graph.remove_node(node)
+    if nodes_removed == 0:
+        return 0
+    else:
+        return nodes_removed + removes_satellites(graph)
+
+
+def betweenness_centrality_intersection(bc_list):
+    """
+    Computes the intersection between the dictionaries  of the betweenness centrality and returns it
+    :param bc_list: list of betweenness centrality dictionaries
+    :return: a the same list where all the nodes outside the intersection are removed
+    """
+    keys_a = bc_list[0].keys()
+    for bc in bc_list:
+        keys_b = bc.keys()
+        keys_a = keys_a & keys_b
+
+    for i in range(len(bc_list)):
+        keys_list = list(bc_list[i].keys())
+        for key in keys_list:
+            if key not in keys_a:
+                del bc_list[i][key]
+
+
+def read_k_conn_from_files(path):
+    """
+    Returns the list of k-vertex connectivity results previously calculated
+    and stored in json files.
+    :param path: folder in which files are
+    :return: the lsit of k-vertex connectivity results
+    """
+    os.chdir(path)
+    snapshot_list = [None] * len(glob.glob('*.json'))
+    for i, file in enumerate(glob.glob('*.json')):
+        with open(file) as f:
+            snapshot_list[i] = json.load(f)
+    return snapshot_list
 
 
 def important_nodes(graph):
@@ -17,7 +91,7 @@ def important_nodes(graph):
     for v, k in betweenness_sorted:
         if k < threshold:
             subgraph.remove_node(v)
-    return subgraph, betweenness_values
+    return subgraph, betweenness_centrality
 
 
 def clean_isolated_nodes(graph):
